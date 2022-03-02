7e60830af725d347ff01ff8802a7e768ef15fdb6
Thomasyong11 <akumyongt27@gmail.com>
2022-03-02 21:04:45 +0000

    final build

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+      Look Up 🔎 2022 Inc, By Adjamkwalim.
+    </div>
+  );
+};
